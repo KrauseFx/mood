f5327dbe193a024e34128f2d5583c93fd3fcc49d
Felix Krause <krausefx@gmail.com>
2018-01-31 23:45:34 -0500

    Add initial storing of values & basic output

--- server.rb
+++ server.rb
@@ -5,15 +5,22 @@ require "json"
 require_relative "./database"
 require_relative "./mail_handler"
 
-class Main < Sinatra::Base
-  enable :logging
+enable :logging
 
-  get "/" do
-    
-  end
+get "/" do
+  db = Mood::Database.database
+  @moods = db[:moods]
+  erb :index
+end
 
-  post "/" do
-    puts "hi"
-    logger.info JSON.pretty_generate(params)
-  end
+post "/" do
+  puts "hi"
+  logger.info JSON.pretty_generate(params)
+
+  # rating = 8 # TODO: replace with actual rating
+  db = Mood::Database.database
+  db[:moods].insert({
+    time: Time.now,
+    value: rating
+  })
 end
